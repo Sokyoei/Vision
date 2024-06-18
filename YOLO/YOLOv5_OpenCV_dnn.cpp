7a24ba05ad1d8da8487e8f215a21cea14581df1e
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2024-06-18 16:27:29 +0800

    fix FindONNXRuntime.cmake

--- YOLO/YOLOv5_OpenCV_dnn.cpp
+++ YOLO/YOLOv5_OpenCV_dnn.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <string>
 
 #include <opencv2/opencv.hpp>
+
+namespace Ahri {
+struct YOLOResult {
+    int class_id;
+    float score;
+    cv::Rect box;
+};
+
+class YOLOv5Detector {
+public:
+    YOLOv5Detector(std::string onnx_path, int height, int width, float threshold_score)
+        : _height(height), _width(width), _threshold_score(threshold_score) {
+        _net = cv::dnn::readNetFromONNX(onnx_path);
+    }
+    ~YOLOv5Detector() {}
+
+    void detect(cv::Mat& frame, std::vector<YOLOResult>& results) {
+        int width = frame.cols;
+        int height = frame.rows;
+        int max_ = std::max(width, height);
+        cv::Mat image = cv::Mat::zeros(cv::Size(max_, max_), CV_8UC3);
+        cv::Rect roi(0, 0, width, height);
+        frame.copyTo(image(roi));
+
+        float x_factor = image.cols / 640.0f;
+        float y_factor = image.rows / 640.0f;
+
+        cv::Mat blob =
+            cv::dnn::blobFromImage(image, 1 / 255.0, cv::Size(_width, _height), cv::Scalar(0, 0, 0), true, false);
+        _net.setInput(blob);
+        cv::Mat pred = _net.forward();
+    }
+
+private:
+    cv::dnn::Net _net;
+    float _threshold_score;
+    int _height;
+    int _width;
+};
+}  // namespace Ahri
 
 int main(int argc, char const* argv[]) {
     return 0;
--- cmake/modules/FindONNXRuntime.cmake
+++ cmake/modules/FindONNXRuntime.cmake
@@ -1,32 +1,54 @@
 # ONNXRuntime
 #
+# Require:
+#   -DONNXRuntime_ROOT_DIR=<your ONNXRuntime root dir>
+#
 # Variables:
-#   ONNXRuntime_INCLUDE_DIR
-#   ONNXRuntime_LIBRARY
+#   ONNXRuntime_INCLUDE_DIRS
+#   ONNXRuntime_LIBRARYS
+#
+# Libraries:
 #   onnxruntime
 #   onnxruntime_providers_shared
+#   with cuda:
+#       onnxruntime_providers_cuda
+#       onnxruntime_providers_tensorrt
 
-unset(ONNXRuntime_INCLUDE_DIR CACHE)
+unset(ONNXRuntime_INCLUDE_DIRS CACHE)
 find_path(
-    ONNXRuntime_INCLUDE_DIR
-    NAMES
-    onnxruntime_c_api.h
-    PATHS ${ONNXRuntime_DIR}/include/
+    ONNXRuntime_INCLUDE_DIRS
+    NAMES onnxruntime_c_api.h onnxruntime_cxx_api.h
+    PATHS ${ONNXRuntime_ROOT_DIR}/include/
 )
 
-unset(ONNXRuntime_LIBRARY CACHE)
-if(WIN32)
-    find_library(onnxruntime NAMES onnxruntime.lib PATHS ${ONNXRuntime_DIR}/lib/)
-    find_library(onnxruntime_providers_shared NAMES onnxruntime_providers_shared.lib PATHS ${ONNXRuntime_DIR}/lib/)
-    set(ONNXRuntime_LIBRARY onnxruntime;onnxruntime_providers_shared)
-elseif(LINUX)
-    find_library(onnxruntime NAMES onnxruntime.so PATHS ${ONNXRuntime_DIR}/lib/)
-    find_library(onnxruntime_providers_shared NAMES onnxruntime_providers_shared.so PATHS ${ONNXRuntime_DIR}/lib/)
-    set(ONNXRuntime_LIBRARY onnxruntime;onnxruntime_providers_shared)
-endif(WIN32)
+unset(ONNXRuntime_LIBRARYS CACHE)
+find_library(onnxruntime NAMES onnxruntime PATHS ${ONNXRuntime_ROOT_DIR}/lib/)
+find_library(onnxruntime_providers_shared NAMES onnxruntime_providers_shared PATHS ${ONNXRuntime_ROOT_DIR}/lib/)
+set(ONNXRuntime_LIBRARYS onnxruntime;onnxruntime_providers_shared)
+if(CMAKE_CUDA_COMPILER)
+    find_library(onnxruntime_providers_cuda NAMES onnxruntime_providers_cuda PATHS ${ONNXRuntime_ROOT_DIR}/lib/)
+    find_library(onnxruntime_providers_tensorrt NAMES onnxruntime_providers_tensorrt PATHS ${ONNXRuntime_ROOT_DIR}/lib/)
+    set(ONNXRuntime_LIBRARYS ${ONNXRuntime_LIBRARYS};onnxruntime_providers_cuda;onnxruntime_providers_tensorrt)
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     ONNXRuntime
-    REQUIRED_VARS ONNXRuntime_INCLUDE_DIR ONNXRuntime_LIBRARY onnxruntime onnxruntime_providers_shared
+    REQUIRED_VARS ONNXRuntime_ROOT_DIR ONNXRuntime_INCLUDE_DIRS ONNXRuntime_LIBRARYS
 )
+mark_as_advanced(ONNXRuntime_ROOT_DIR ONNXRuntime_INCLUDE_DIRS ONNXRuntime_LIBRARYS)
+
+if(ONNXRuntime_FOUND)
+    if(NOT TARGET ONNXRuntime::ONNXRuntime)
+        add_library(ONNXRuntime::ONNXRuntime INTERFACE IMPORTED)
+        target_include_directories(ONNXRuntime::ONNXRuntime INTERFACE ${ONNXRuntime_INCLUDE_DIRS})
+        target_link_directories(ONNXRuntime::ONNXRuntime INTERFACE ${ONNXRuntime_ROOT_DIR}/lib)
+        target_link_libraries(ONNXRuntime::ONNXRuntime INTERFACE ${ONNXRuntime_LIBRARYS})
+        # set_target_properties(
+        #     ONNXRuntime::ONNXRuntime PROPERTIES
+        #     INTERFACE_LINK_DIRECTORIES  ${ONNXRuntime_ROOT_DIR}/lib
+        #     INTERFACE_LINK_LIBRARIES ${ONNXRuntime_LIBRARYS}
+        #     INTERFACE_INCLUDE_DIRECTORIES ${ONNXRuntime_INCLUDE_DIRS}
+        # )
+    endif()
+endif()
