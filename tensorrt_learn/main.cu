1d0a49ab5f2a31cb195c43ceb4ae1b57f4d4993e
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-04-10 17:21:18 +0800

    fix bug

--- tensorrt_learn/main.cu
+++ tensorrt_learn/main.cu
@@ -1,17 +1,38 @@
 #include <string>
 #include <vector>
 
+#ifdef _WIN32
+#include <Windows.h>
+#elif defined(__linux__)
+#include <dlfcn.h>
+#endif
+
 #include <NvInfer.h>
 #include <NvInferPlugin.h>
 #include <NvInferVersion.h>
 #include <cuda_runtime.h>
 
+// #include "tensorrt_learn/plugins/ahri_leakyrelu_tensorrt.cuh"
+// #include "tensorrt_learn/plugins/ahri_swish_tensorrt.cuh"
 #include "tensorrt_utils.cuh"
 
 int main(int argc, char const* argv[]) {
-    auto model = Ahri::TensorRT::Model("/media/supervisor/windowsd/Code/github/Vision/tensorrt_learn/ahrinet.onnx");
+    initLibNvInferPlugins(&Ahri::TensorRT::trtlogger, "");
+
+#ifdef _WIN32
+#elif defined(__linux__)
+    void* ahri_plugin_tensorrt = dlopen("libahri_plugin_tensorrt.so", RTLD_LAZY);
+#endif
+
+    auto model =
+        Ahri::TensorRT::TensorRTModel("/media/supervisor/windowsd/Code/github/Vision/tensorrt_learn/ahrinet.onnx");
     model.build();
     model.infer();
+
+#ifdef _WIN32
+#elif defined(__linux__)
+    dlclose(ahri_plugin_tensorrt);
+#endif
 
     return 0;
 }
