bf9829a9d266f5bff4af63d51a4fbb49f943dc4b
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-08-06 02:18:01 +0800

    update tensorrt_learn for windows

--- tensorrt_learn/README.md
+++ tensorrt_learn/README.md
@@ -1,7 +1,25 @@
 # TensorRT
 
+## 流程
+
+- Logger    日志记录器
+- Builder   用于创建 Network, 对模型序列化生成 engine
+- Network   由 Builder 创建，最初是一个空容器
+- Parser    用于解析 onnx 模型
+- Context   上接 engine, 下接 inference
+
+## 量化
+
+- PTQ
+- QAT
+
 ## .onnx 转换 .engine
 
 ```shell
 trtexec --onnx=onnx_file --saveEngine=engine_file
 ```
+
+## 参考
+
+- [trt-samples-for-hackathon-cn](https://github.com/NVIDIA/trt-samples-for-hackathon-cn)
+- [wang-xinyu/tensorrtx](https://github.com/wang-xinyu/tensorrtx)
--- tensorrt_learn/main.cpp
+++ tensorrt_learn/main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include <string>
 #include <vector>
+
+#include "Vision.hpp"
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -12,16 +15,21 @@
 #include <NvInferVersion.h>
 #include <cuda_runtime.h>
 
-// #include "tensorrt_learn/plugins/ahri_leakyrelu_tensorrt.cuh"
-// #include "tensorrt_learn/plugins/ahri_swish_tensorrt.cuh"
 #include "Ahri/Vision/tensorrt_utils.hpp"
-
-#include "Vision.h"
 
 int main(int argc, char const* argv[]) {
     initLibNvInferPlugins(&Ahri::TensorRT::trtlogger, "");
 
 #ifdef _WIN32
+    auto ahri_plugin_tensorrt = LoadLibraryEx(TEXT("ahri_plugin_tensorrt.dll"), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+    if (!ahri_plugin_tensorrt) {
+        // 处理加载失败的情况
+        DWORD error = GetLastError();
+        std::cerr << "Failed to load plugin library. Error: " << error << std::endl;
+        return 1;
+    } else {
+        std::cout << "Load DLL success" << std::endl;
+    }
 #elif defined(__linux__)
     void* ahri_plugin_tensorrt = dlopen("libahri_plugin_tensorrt.so", RTLD_LAZY);
 #endif
@@ -31,6 +39,7 @@ int main(int argc, char const* argv[]) {
     model.infer();
 
 #ifdef _WIN32
+    FreeLibrary(ahri_plugin_tensorrt);
 #elif defined(__linux__)
     dlclose(ahri_plugin_tensorrt);
 #endif
