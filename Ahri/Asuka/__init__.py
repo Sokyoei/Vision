703767c3e65049f92ce40c20117835bb59ed1709
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2026-01-07 17:50:57 +0800

    move Ahri/Vision to Ahri/Asuka

---
+++ Ahri/Asuka/__init__.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+ASUKA_ROOT = Path(__file__).resolve().parent.parent.parent
+
+sokyoei_data_dir = os.environ.get('SOKYOEI_DATA_DIR')
+if sokyoei_data_dir:
+    SOKYOEI_DATA_DIR = Path(sokyoei_data_dir)
+else:
+    raise RuntimeError("Please install https://github.com/Sokyoei/data")
