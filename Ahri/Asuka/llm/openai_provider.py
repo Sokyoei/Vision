2c8064d5294560aeae1defa73ce728f75d449c39
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2026-01-08 16:44:18 +0800

    add llm openai provider

---
+++ Ahri/Asuka/llm/openai_provider.py
@@ -0,0 +1,47 @@
+from openai import AsyncOpenAI, OpenAI
+
+
+def get_messages(message: str):
+    return [{'role': 'system', 'content': 'You are a helpful assistant.'}, {'role': 'user', 'content': message}]
+
+
+class OpenAIProvider(object):
+
+    def __init__(self, url: str, api_key: str, model: str):
+        self.url = url
+        self.api_key = api_key
+        self.model = model
+        self.client = OpenAI(api_key=api_key, base_url=url)
+
+    def text(self, message: str):
+        completion = self.client.chat.completions.create(model=self.model, messages=get_messages(message))
+        return completion.choices[0].message.content
+
+    def text_stream(self, message: str):
+        completion = self.client.chat.completions.create(model=self.model, messages=get_messages(message), stream=True)
+        for chunk in completion:
+            if chunk.choices and chunk.choices[0].delta.content:
+                content = chunk.choices[0].delta.content
+                yield content
+
+
+class AsyncOpenAIProvider(object):
+
+    def __init__(self, url: str, api_key: str, model: str):
+        self.url = url
+        self.api_key = api_key
+        self.model = model
+        self.client = AsyncOpenAI(api_key=api_key, base_url=url)
+
+    async def text(self, message: str):
+        completion = await self.client.chat.completions.create(model=self.model, messages=get_messages(message))
+        return completion.choices[0].message.content
+
+    async def text_stream(self, message: str):
+        completion = await self.client.chat.completions.create(
+            model=self.model, messages=get_messages(message), stream=True
+        )
+        async for chunk in completion:
+            if chunk.choices and chunk.choices[0].delta.content:
+                content = chunk.choices[0].delta.content
+                yield content
